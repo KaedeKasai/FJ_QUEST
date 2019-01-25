c654f170a2731ff7e86ce155ee5abbaef774986e
Kazu555 <43734605+Kazu555@users.noreply.github.com>
2019-01-25 10:11:56 +0900

    ゲームシステムの開発
    ・テラインの編集
    ・敵のナビゲート処理
    ・タイトル画面の作成
    etc...

---
+++ Assets/SampleScenes/Scripts/GameSystem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+ 
+public class GameSystem : MonoBehaviour {
+ 
+	//　スタートボタンを押したら実行する
+	public void OnClick() {
+		SceneManager.LoadScene (0);
+	}
+    
+    public void end() {
+		Application.Quit();
+	}
+}
